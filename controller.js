f42552e8f1efc72f39d9a139251a8dab136825ea
rifardo <brongkeinyong@gmail.com>
2022-05-05 18:34:31 +0700

    success showdata all customer

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok('application REST API running!')
+    response.ok('application REST API running!',res)
+}; 
+
+//show data cust
+exports.showdata = function(req,res){
+    connection.query('SELECT * FROM `tb_customer`', function(error,rows,fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows,res);
+        }
+    });
 };
